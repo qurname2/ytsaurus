60fcb599bfcb98a1731c63ec22cdea20de29a890
Ignat Kolesnichenko <ignat@yandex-team.ru>
2024-03-02 13:53:39 +0000

    Test ytsaurus-yson package build

---
+++ yt/yt/scripts/build_python_libraries_with_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+
+mkdir -p ../ya_build
+rm -rf ../ya_build/*
+
+YA_BUILD_PATH=$(realpath $(pwd)/../ya_build)
+
+container_id=$(docker container run --name yson-bindings-build --entrypoint /bin/bash -v $(pwd):/ytsaurus -v $YA_BUILD_PATH:/ya_build -dit quay.io/pypa/manylinux2014_x86_64)
+
+docker exec -it $container_id /ytsaurus/yt/yt/scripts/build_manylinux_yson_bindings.sh
