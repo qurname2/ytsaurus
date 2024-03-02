010a1a16ac92e375278e0e776b697b304a7751e4
Ignat Kolesnichenko <ignat@yandex-team.ru>
2024-03-02 19:03:03 +0000

    Fix paths

--- yt/yt/scripts/build_python_libraries_with_docker.sh
+++ yt/yt/scripts/build_python_libraries_with_docker.sh
@@ -5,6 +5,6 @@ rm -rf ../ya_build/*
 
 YA_BUILD_PATH=$(realpath $(pwd)/../ya_build)
 
-container_id=$(docker container run --name yson-bindings-build --entrypoint /bin/bash -v $(pwd):/ytsaurus -v $YA_BUILD_PATH:/ya_build -dit quay.io/pypa/manylinux2014_x86_64)
+container_id=$(docker container run --name yson-bindings-build --entrypoint /bin/bash -v $(pwd)/ytsaurus:/ytsaurus -v $YA_BUILD_PATH:/ya_build -dit quay.io/pypa/manylinux2014_x86_64)
 
 docker exec -it $container_id /ytsaurus/yt/yt/scripts/build_manylinux_yson_bindings.sh
