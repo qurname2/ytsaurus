4855ebca81af9afc3ac5523d0b2fe73c2418ebb3
Ignat Kolesnichenko <ignat@yandex-team.ru>
2024-03-02 19:20:01 +0000

    Another fix

--- yt/yt/scripts/build_python_libraries_with_docker.sh
+++ yt/yt/scripts/build_python_libraries_with_docker.sh
@@ -7,4 +7,4 @@ YA_BUILD_PATH=$(realpath $(pwd)/../ya_build)
 
 container_id=$(docker container run --name yson-bindings-build --entrypoint /bin/bash -v $(pwd)/ytsaurus:/ytsaurus -v $YA_BUILD_PATH:/ya_build -dit quay.io/pypa/manylinux2014_x86_64)
 
-docker exec -it $container_id /ytsaurus/yt/yt/scripts/build_manylinux_yson_bindings.sh
+docker exec $container_id /ytsaurus/yt/yt/scripts/build_manylinux_yson_bindings.sh
